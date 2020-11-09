ebd75e863c7ca4fa2abd2b5048b34224d7d087c1
zengcaimei <1483853871@qq.com>
2020-11-09 17:10:00 +0800

    首次提交

---
+++ src/views/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="content">
+		<p>vue3.0脚手架封装</p>
+	</div>
+</template>
+
+<script>
+	import {
+		getListAPI
+	} from '@/api/api'
+	export default {
+		data(){
+			return{
+				
+			}
+		},
+		created() {
+			this.getList()
+		},
+		methods:{
+			getList(){
+				getListAPI().then(res=>{
+					console.log(res)
+				})
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.content{
+		p{
+			text-align: center;
+		}
+	}
+</style>
